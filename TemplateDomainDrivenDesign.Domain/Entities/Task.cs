73386836036b37b0d1ddb0654433ad5df40a55c2
Frederic Merouze <frederic.merouze@gmail.com>
2023-01-02 17:12:03 +0100

    [UPDATE] get entity abstract class for all domain entities

--- TemplateDomainDrivenDesign.Domain/Entities/Task.cs
+++ TemplateDomainDrivenDesign.Domain/Entities/Task.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TemplateDomainDrivenDesign.Domain.Primitives;
 
 namespace TemplateDomainDrivenDesign.Domain.Entities
 {
-    public class Task
+    public sealed class Task : Entity
     {
-        public int Id { get; set; }
+        public Task(Guid id, string description) : base(id)
+        {
+            Description = description;
+        }
         public string Description { get; set; }
     }
 }
---
+++ TemplateDomainDrivenDesign.Domain/Primitives/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateDomainDrivenDesign.Domain.Primitives
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+        protected Entity() { }
+
+        protected Entity(Guid id) => Id = id;
+    }
+}
