ce413cc28c1e1f44ee0f534963248ca9bd2c3c26
Frederic Merouze <frederic.merouze@gmail.com>
2023-01-03 09:04:08 +0100

    [ADD] add custom exception to the domain layer

---
+++ TemplateDomainDrivenDesign.Domain/Exceptions/Base/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateDomainDrivenDesign.Domain.Exceptions.Base
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() { }
+        public NotFoundException(string message) : base(message) { }
+        public NotFoundException(string message, Exception inneException) : base(message, inneException) { }
+    }
+}
