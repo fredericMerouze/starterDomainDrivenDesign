89eff08bd2e58ab23fb5a6e1a019121f9bf08254
Arthur <a.foutrel07@gmail.com>
2023-06-13 17:49:10 +0200

    [ADD] Unit test for GetAllTaskForUser

---
+++ TemplateDomainDrivenDesign.UnitTests/Features/Task/TaskRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using CleanArchitecture.Domain.Contracts.Repositories;
+using CleanArchitecture.Persistence.Repositories;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CleanArchitecture.UnitTests.Features.Task;
+
+public class TaskRepositoryTest
+{
+    [Fact]
+    public void ShouldReturnListOfTasks()
+    {
+        // Arrange
+        var repository = new Mock<ITaskRepository>();
+        Guid guid = Guid.NewGuid();
+        List<Domain.Entities.Task> tasks = new List<Domain.Entities.Task> {
+            new Domain.Entities.Task(guid, "Ma desc"),
+            new Domain.Entities.Task(Guid.NewGuid(), "Autre desc")
+        };
+        repository.Setup(x => x.GetAllTaskForUser()).Returns(tasks);
+        // Act
+        var results = repository.Object.GetAllTaskForUser();
+        // Assert
+        results.Should().HaveCount(2);
+        results.Should().BeOfType<List<Domain.Entities.Task>>();
+        results.First().Description.Should().Be("Ma desc");
+        results.First().Id.Should().Be(guid);
+    }
+}
